914f66c53375eb5bf09f5769bf79b4bfb8abed50
Ryan Doucette <31770773+RyanJLDoucette@users.noreply.github.com>
2018-08-21 15:28:05 -0300

    Done
    Did alot of things and forgot to push them as I went along (oops)

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
+/*
+This takes in a number that represents an amount of cents. It will calculate how many of what coin would compromise this number and then dispays
+that to the user. Uses 2 helper function: clear(), displayCoins()
+*/
 function makeChange() {
-    var money;
+    clear();//I first clear in case this function was used previously and there are previously results already displayed on the screen
+
+    var money;//money is the number of cents that the user wrote in the text field
     money = document.getElementById("change").value;
     
-    
-    var numLoonies;
-    var numQuarters;
-    var numDimes;
-    var numNickels;
-    var numPennies;
+    //We need to display the number of each coin returned.
+    var numLoonies = 0;
+    var numQuarters = 0;
+    var numDimes = 0;
+    var numNickels = 0;
+    var numPennies = 0;
     
 
     do {
-        console.log(money);
         if(money >= 100) {
             numLoonies++;
             money -= 100;
@@ -35,7 +40,57 @@ function makeChange() {
     }
     while(money > 0)
 
-    document.getElementById("test").innerHTML = money;
+    displayCoins(numPennies, numNickels, numDimes, numQuarters, numLoonies);//see function displayCoins(params)
+}
 
+/*
+Helper function of make change
+This function clears the images and text off the screen. It is needed because if the user presses the make change button. The results will remain there
+from the previous click.
+*/
+function clear() {
+    document.getElementById("1-cent-coin-block").style.display = "none";
+    document.getElementById("5-cent-coin-block").style.display = "none";
+    document.getElementById("10-cent-coin-block").style.display = "none";
+    document.getElementById("25-cent-coin-block").style.display = "none";
+    document.getElementById("100-cent-coin-block").style.display = "none";
+}
 
+/*
+Helper function of make change
+INPUT:number of each coin
+OUTPUT: displays how many of each coin is needed and if it is needed will display the corresponding coins image
+*/
+function displayCoins(numPennies, numNickels, numDimes, numQuarters, numLoonies) {
+    var numCoins = 0;
+
+    if(numPennies > 0) {
+        numCoins = numPennies;
+        document.getElementById("1-cent-coin-block").style.display = "block";
+        document.getElementById("1-cent-coin-p").innerHTML = (numPennies.toString()).concat(" x 1");
+    }
+
+    if(numNickels > 0) {
+        numCoins = numNickels;
+        document.getElementById("5-cent-coin-block").style.display = "block";
+        document.getElementById("5-cent-coin-p").innerHTML = (numNickels.toString()).concat(" x 5");
+    }
+
+    if(numDimes > 0) {
+        numCoins = numDimes;
+        document.getElementById("10-cent-coin-block").style.display = "block";
+        document.getElementById("10-cent-coin-p").innerHTML = (numDimes.toString()).concat(" x 10");
+    }
+
+    if(numQuarters > 0) {
+        numCoins = numQuarters;
+        document.getElementById("25-cent-coin-block").style.display = "block";
+        document.getElementById("25-cent-coin-p").innerHTML = (numQuarters.toString()).concat(" x 25");
+    }
+
+    if(numLoonies > 0) {
+        numCoins = numLoonies;
+        document.getElementById("100-cent-coin-block").style.display = "block";
+        document.getElementById("100-cent-coin-p").innerHTML = (numLoonies.toString()).concat(" x 100");
+    }
 }
